52e7fabb0c3248c5545f58ee83667ca2ac30fb1f
Eric Dehau <dehau@users.noreply.github.com>
2016-04-19 16:28:35 +0200

    fix pdf generation error when calling phantomJS without filename extension pdf

--- pdf/export.js
+++ pdf/export.js
@@ -1,27 +1,26 @@
 var fs = require('fs');
-var node_path = require('path');
+var path = require('path');
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs-prebuilt');
 var tmp = require('tmp');
 
 var binPath = phantomjs.path;
 
-module.exports = function(url, filename) {
-  filename = filename || 'export.pdf';
+module.exports = function(url) {
   return new Promise(function(res, rej) {
-    tmp.dir(function _tempDirCreated(err, path, cleanupCallback) {
+    tmp.dir(function _tempDirCreated(err, tmpDir) {
       if (err) {
         return rej(err);
       }
-      path = path + '/' + filename;
-      var childArgs = [ node_path.join(__dirname, 'phantom_export.js'), url, path ]
+      tmpFile = tmpDir + '/tmpName.pdf';
+      var childArgs = [ path.join(__dirname, 'phantom_export.js'), url, tmpFile ];
       childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
         if (err) {
           err.stdout = stdout;
           err.stderr = stderr;
           return rej(err);
         }
-        res(fs.createReadStream(path));
+        res(fs.createReadStream(tmpFile));
       })
     });
   });
