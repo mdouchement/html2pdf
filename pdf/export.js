944a962066573d8ea76798035d6a4553626dcaf8
Sylvain <sylvain.ragot@predicsis.com>
2016-04-19 16:17:51 +0200

    Check filename extension

--- pdf/export.js
+++ pdf/export.js
@@ -6,8 +6,43 @@ var tmp = require('tmp');
 
 var binPath = phantomjs.path;
 
+/**
+ * Calling export function with a filename that have no extension fails. 
+ * So, we must ensure the file has a valid extension. To do that, we check if :
+ * - the filename string contains the extension and this substring 
+ * - the extension ends the string 
+ *
+ * hasExtention('test', '.pdf') 
+ *   => false
+ *
+ * hasExtention('test.pdf.txt', '.pdf') 
+ *   => false
+ * 
+ * hasExtention('.pdf', '.pdf') 
+ *   => false
+ * 
+ * hasExtention(undefined, '.pdf') 
+ *   => false
+ * 
+ * hasExtention('test.pdf', '.pdf') 
+ *   => true
+ * 
+ * @param String filename 
+ * @param String extension
+ * @returns Boolean
+ */
+function hasExtention(filename, extension) {
+  return filename.length > extension.length
+    && filename.lastIndexOf(extension) >= 0 
+    && filename.lastIndexOf(extension) + extension.length === filename.length;
+}  
+
 module.exports = function(url, filename) {
   filename = filename || 'export.pdf';
+  filename = (hasExtention(filename, '.pdf'))
+    ? filename
+    : filename + '.pdf';
+
   return new Promise(function(res, rej) {
     tmp.dir(function _tempDirCreated(err, path, cleanupCallback) {
       if (err) {
