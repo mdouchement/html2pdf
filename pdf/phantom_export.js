fb019f1b1247ac616cad33a2b8f185e217181e8c
Eric Dehau <dehau@users.noreply.github.com>
2016-04-22 13:36:59 +0200

    wait for all graphs to be loaded before rendering pdf

--- pdf/phantom_export.js
+++ pdf/phantom_export.js
@@ -10,22 +10,46 @@ page.viewportSize = { width: 1238, height: 1763 };
 page.paperSize = {width:'1238px', height:'1763px'};
 page.settings.dpi = 100;
 
+function waitForAjax(cb) {
+  var intervalId = setInterval(function() {
+    var loaded = page.evaluate(function() {
+      return !Boolean(jQuery) || !Boolean(jQuery.active);
+    });
+    if (loaded) {
+      cb();
+    }
+  }, 2000)
+}
+
 page.onError = function (msg, trace) {
   console.error(msg);
   trace.forEach(function(item) {
     console.error('  ', item.file, ':', item.line);
   });
-  phantom.exit(-1);
+  phantom.exit(1);
+};
+
+page.onResourceError = function(resourceError) {
+  console.error('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+  console.error('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+  phantom.exit(1);
+};
+
+page.onResourceTimeout = function(request) {
+  console.error('Response (#' + request.id + '): ' + JSON.stringify(request));
+  phantom.exit(1);
 };
 
 page.open(url, function (status) {
   if (status === 'success') {
-    setTimeout(function() {
-      page.render(path);
-      phantom.exit();
-    }, 1000);
+    waitForAjax(function() {
+      setTimeout(function() {
+        page.render(path, { format: 'pdf' });
+        phantom.exit();
+      }, 500);
+    });
   } else {
     console.error('network error', url);
-    phantom.exit(-1);
+    phantom.exit(1);
   }
 });
